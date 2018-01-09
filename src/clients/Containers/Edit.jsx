5959bcd853c104fd23ba4e46792c5683b9429fd6
Alex Rossi Ignez <rx.rossi@gmail.com>
2018-01-09 02:21:58 +0000

    Made changes to containers edit, not working yet

--- src/clients/Containers/Edit.jsx
+++ src/clients/Containers/Edit.jsx
@@ -12,8 +12,8 @@ class Edit extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount() {
-    console.log(this.props.clientId);
+  componentWillMount() {
+    this.props.getClients();
     // place asyncActions.get
 
     // if state.clients.formFields.update.id !== this.props.clientId
@@ -27,7 +27,12 @@ class Edit extends React.Component {
     // Form should have accept a prop of edit or update method
     // So it can addapt the texts at: Title and button
 
-    // server should send id and not just _id for clients and others
+    // Probably containers will be reusable, think if it is possible
+  }
+
+  componentDidMount() {
+    const client = this.props.clients.find(x => x.id === this.props.clientId);
+    this.props.setFields(client);
   }
 
   handleChange(name) {
@@ -59,22 +64,33 @@ Edit.propTypes = {
   changeField: PropTypes.func.isRequired,
   clearFields: PropTypes.func.isRequired,
   editClient: PropTypes.func.isRequired,
+  getClients: PropTypes.func.isRequired,
+  setFields: PropTypes.func.isRequired,
   clientId: PropTypes.string.isRequired,
+  clients: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]))).isRequired,
 };
 
 
 const { asyncActions, updateFormFieldActions } = reusableReduxConfig(urls.CLIENTS, 'clients');
 const formActions = updateFormFieldActions;
 
+console.log(formActions);
+
 const mapDispatch = {
   editClient: asyncActions.put,
+  getClients: asyncActions.get,
   changeField: formActions.changeField,
   clearFields: formActions.clear,
+  setFields: formActions.set,
 };
 
 const mapState = state => ({
   errors: state.clients.APIStatus.put.errors,
   values: state.clients.formFields.update,
+  clients: state.clients.entities,
 });
 
 export { Edit };
