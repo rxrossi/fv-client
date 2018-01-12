3559de0f53ce0b648497c7f231735292e8fe5e64
rxrossi <rx.rossi@gmail.com>
2018-01-12 23:33:55 +0000

    Created HOC Create

---
+++ src/HOC/Create.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default (Component) => {
+  class CreateHOC extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.handleReset = this.handleReset.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleAppendField = this.handleAppendField.bind(this);
+      this.handleRemoveField = this.handleRemoveField.bind(this);
+    }
+
+    handleAppendField(path, value) {
+      this.props.appendField(path, value);
+    }
+
+    handleRemoveField(path) {
+      this.props.removeField(path);
+    }
+
+    handleSubmit(e) {
+      e.preventDefault();
+      const { submit, fieldValues } = this.props;
+      submit(fieldValues);
+    }
+
+    handleChange(name) {
+      return e => this.props.changeField(name, e.target.value);
+    }
+
+    handleReset() {
+      this.props.clearFields();
+    }
+
+    render() {
+      const { fieldValues, errors } = this.props;
+
+      return (
+        <Component
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          handleReset={this.handleReset}
+          values={fieldValues}
+          errors={errors}
+        />
+      );
+    }
+  }
+
+  CreateHOC.propTypes = {
+    submit: PropTypes.func.isRequired,
+    clearFields: PropTypes.func.isRequired,
+    changeField: PropTypes.func.isRequired,
+    appendField: PropTypes.func,
+    removeField: PropTypes.func,
+    fieldValues: PropTypes.objectOf(PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.arrayOf(PropTypes.objectOf(PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+      ]))),
+    ])).isRequired,
+    errors: PropTypes.objectOf(PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.arrayOf(PropTypes.objectOf(PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+      ]))),
+    ])).isRequired,
+  };
+
+  CreateHOC.defaultProps = {
+    appendField: (() => {}),
+    removeField: (() => {}),
+  };
+
+
+  return CreateHOC;
+};
+
---
+++ src/HOC/Create.test.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, mount } from 'enzyme';
+import createHOC from './Create';
+// Configure Enzyme
+configure({ adapter: new Adapter() });
+
+const SomePresentational = () => (
+  <p>Presentational component</p>
+);
+
+function mountComponent({
+  changeField,
+  appendField,
+  removeField,
+  fieldValues,
+  clearFields,
+  errors,
+  submit,
+} = {}) {
+  const Create = createHOC(SomePresentational);
+
+  return mount(<Create
+    changeField={changeField || (() => {})}
+    appendField={appendField || (() => {})}
+    removeField={removeField || (() => {})}
+    fieldValues={fieldValues || {}}
+    errors={errors || {}}
+    clearFields={clearFields || (() => {})}
+    submit={submit || (() => {})}
+  />);
+}
+
+describe('Create HOC', () => {
+  it('mounts and has the presentational component', () => {
+    // Prepare
+    // Act
+    const sut = mountComponent();
+    // Assert
+    expect(sut.find(SomePresentational).length).toBe(1);
+  });
+
+  describe('Changing a field', () => {
+    it('calls the correct prop when the presentational component calls its handleMethod', () => {
+      // Prepare
+      const changeField = jest.fn();
+      const sut = mountComponent({ changeField });
+      // Act
+      sut.instance().handleChange('name')({ target: { value: 'someVal' } });
+      // Assert
+      expect(changeField).toHaveBeenCalledTimes(1);
+      expect(changeField).toHaveBeenCalledWith('name', 'someVal');
+    });
+
+    it('passes handleChange method to Presentational', () => {
+      // Prepare
+      const sut = mountComponent();
+      // Assert
+      expect(typeof sut.find(SomePresentational).props().handleChange).toBe('function');
+    });
+  });
+
+  describe('Appending a dynamic field', () => {
+    it('calls the correct prop', () => {
+      // Prepare
+      const appendField = jest.fn();
+      const sut = mountComponent({ appendField });
+      const nameOfArray = 'name of array';
+      const objectToAppend = {};
+      // Act
+      sut.instance().handleAppendField(nameOfArray, objectToAppend);
+      // Assert
+      expect(appendField).toHaveBeenCalledWith(nameOfArray, objectToAppend);
+    });
+  });
+
+  describe('Removing a dynamic field', () => {
+    it('calls the correct prop', () => {
+      // Prepare
+      const removeField = jest.fn();
+      const sut = mountComponent({ removeField });
+      const path = 'some path';
+      // Act
+      sut.instance().handleRemoveField(path);
+      // Assert
+      expect(removeField).toHaveBeenCalledWith(path);
+    });
+  });
+
+  describe('Clearing the fields and errors', () => {
+    it('calls the correct prop', () => {
+      // Prepare
+      const clearFields = jest.fn();
+      const sut = mountComponent({ clearFields });
+
+      // Act
+      sut.instance().handleReset();
+
+      // Assert
+      expect(clearFields).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('POST method', () => {
+    it('calls the correct prop function with the correct values', () => {
+      // Prepare
+      const submit = jest.fn();
+      const fieldValues = { id: 'e2', name: 'name2' };
+      const sut = mountComponent({ fieldValues, submit });
+
+      const evt = { preventDefault: () => {} };
+      // Act
+      sut.instance().handleSubmit(evt);
+
+      // Assert
+      expect(submit).toHaveBeenCalledWith(fieldValues);
+    });
+  });
+});
