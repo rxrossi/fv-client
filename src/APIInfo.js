f87ae19125e4271edb3afee652787db559c57d10
rxrossi <rx.rossi@gmail.com>
2018-01-27 12:07:16 +0000

    Configured to use heroku api

--- src/APIInfo.js
+++ src/APIInfo.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5001';
+export const BASE_URL = 'https://rxmanager-backend.herokuapp.com';
 export const CLIENTS = `${BASE_URL}/clients`;
 export const PRODUCTS = `${BASE_URL}/products`;
 export const PROFESSIONALS = `${BASE_URL}/professionals`;
